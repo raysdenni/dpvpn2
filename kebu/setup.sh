c186072b1cfbc22d31382225595218bf6e34c9ae
dpvpn09 <92319385+dpvpn09@users.noreply.github.com>
2021-10-19 06:47:27 +0700

    Update setup.sh

--- kebu/setup.sh
+++ kebu/setup.sh
@@ -47,7 +47,7 @@ wget https://raw.githubusercontent.com/dpvpn09/dpvpn2/main/kebu/sodosok.sh && ch
 #install wireguard
 wget https://raw.githubusercontent.com/dpvpn09/dpvpn2/main/kebu/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
-wget https://raw.githubusercontent.com/dpvpn09/dpvpn/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/dpvpn09/dpvpn2/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 #install L2TP
 wget https://raw.githubusercontent.com/dpvpn09/dpvpn2/main/kebu/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 #wget https://raw.githubusercontent.com/YakkoRasZ14/private/main/kebu/set-br.sh && chmod +x set-br.sh && ./set-br.sh
